bc98f923a55c88d020148bf9b3a6356c5bf23b93
Bestian Tang <bestian@gmail.com>
2021-01-07 10:23:27 +0800

    deploy

---
+++ precache-manifest.87d24c880e5e6d5e7ff1bd1a4be01a1e.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b47a11291e0b4699e2d",
+    "url": "/css/about.9f4d9ab1.css"
+  },
+  {
+    "revision": "9b47a11291e0b4699e2d",
+    "url": "/css/about.9f4d9ab1.css.map"
+  },
+  {
+    "revision": "39c6a8eca48149988c3d",
+    "url": "/css/app.0810fa6c.css"
+  },
+  {
+    "revision": "39c6a8eca48149988c3d",
+    "url": "/css/app.0810fa6c.css.map"
+  },
+  {
+    "revision": "1473dfe02c6b862e3c81c3725c4dafdf",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "2f3bbb7fb0a0263497714807b9fa0b2d",
+    "url": "/img/cards/Club_Jack.jpg"
+  },
+  {
+    "revision": "685f0a6eacd19d0896201d63868a6980",
+    "url": "/img/cards/Club_Queen.png"
+  },
+  {
+    "revision": "ebd56d59070ff84002af3265c438a562",
+    "url": "/img/cards/Club_king.png"
+  },
+  {
+    "revision": "04279a9cb63d9f5d4311496180be76ec",
+    "url": "/img/cards/Dimond_Jack.png"
+  },
+  {
+    "revision": "e911d127a0e80fabe2d8deb299a8b112",
+    "url": "/img/cards/Dimond_King.png"
+  },
+  {
+    "revision": "41bb6ed91d7573a31f48518eb8c2af21",
+    "url": "/img/cards/Dimond_Queen.png"
+  },
+  {
+    "revision": "da941602cfdbda5aea9011038f7b36df",
+    "url": "/img/cards/Dimond_king.jpg"
+  },
+  {
+    "revision": "eff2dcfe2a3331bc93d1c097285fed0a",
+    "url": "/img/cards/Heart_Jack.png"
+  },
+  {
+    "revision": "f8beeb45bced8d0d343674c4ffa71bb3",
+    "url": "/img/cards/Heart_Queen.png"
+  },
+  {
+    "revision": "79bfa5f98fe9bb930be4cf45b399f9a2",
+    "url": "/img/cards/Heart_king.jpeg"
+  },
+  {
+    "revision": "cda2013e14a2f4cc19b2db5a85800d57",
+    "url": "/img/cards/Jocker1.jpg"
+  },
+  {
+    "revision": "468ba357216bcae0b9cc6ac744a1e697",
+    "url": "/img/cards/Jocker2.jpg"
+  },
+  {
+    "revision": "2517e0a1d37d618d43896f72b925b6ce",
+    "url": "/img/cards/Spade_Jack.jpg"
+  },
+  {
+    "revision": "eb82e226f347730b90f83c7eb371416a",
+    "url": "/img/cards/Spade_Queen.png"
+  },
+  {
+    "revision": "324e98ed2d75467528b8a118216df81c",
+    "url": "/img/cards/Spade_king.png"
+  },
+  {
+    "revision": "7fa21d7392ba94eab9a33993bedf33b6",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "372cccc5b4c80f0c0e2ca49ecac7073a",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "7fa21d7392ba94eab9a33993bedf33b6",
+    "url": "/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "372cccc5b4c80f0c0e2ca49ecac7073a",
+    "url": "/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "165632f45cef5f279747e90cfa592ae3",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "ae01b05fa5418c686816087770827de7",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "797636564aa681cc53b740a63f5efa87",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9f1a97e9499100ecd70144f35d883d04",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "9a0a2fdcaf3dbe925f33683ff00ca0a6",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "797636564aa681cc53b740a63f5efa87",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "b10e760f5e94473c37a7755f8d74ff87",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "b9893587510cb80a9024faeae491d6de",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "1473dfe02c6b862e3c81c3725c4dafdf",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "ca9b72d123a80d2915c1292b975dfec1",
+    "url": "/img/icons/manifest.json"
+  },
+  {
+    "revision": "8291cb8f5d9eacb0e883daf4f2a61677",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "671ade0715dbb402abbd8fdbaa7fce02",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "07be93352026daf5d8b0bcd87b675a6a",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "6f79971dcfca51ca679bb17484b157f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b47a11291e0b4699e2d",
+    "url": "/js/about.3cab2363.js"
+  },
+  {
+    "revision": "9b47a11291e0b4699e2d",
+    "url": "/js/about.3cab2363.js.map"
+  },
+  {
+    "revision": "39c6a8eca48149988c3d",
+    "url": "/js/app.b078fdc4.js"
+  },
+  {
+    "revision": "39c6a8eca48149988c3d",
+    "url": "/js/app.b078fdc4.js.map"
+  },
+  {
+    "revision": "089d62a496df0d6a6ba0",
+    "url": "/js/chunk-vendors.d58f2e15.js"
+  },
+  {
+    "revision": "089d62a496df0d6a6ba0",
+    "url": "/js/chunk-vendors.d58f2e15.js.map"
+  },
+  {
+    "revision": "dc8ce45fb90135dd3a04449f0477089e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
